194ae425d2bd908dd9d1064d3a358ac96029992f
xPumaa <35280763+xPumaa@users.noreply.github.com>
2022-06-08 21:22:50 +0200

    Complete rework of the plugin Added "Set Target Button" to the priority list

---
+++ AutoJailMarker/Hooks/ActionEffectHook.cs
@@ -0,0 +1,94 @@
+﻿using Dalamud.Hooking;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using AutoJailMarker.Classes;
+
+namespace AutoJailMarker.Hooks;
+
+public unsafe class ActionEffectHook : IDisposable
+{
+    private readonly Hook<ReceiveActionEffectDelegate> receiveActionEffectHook;
+
+    private delegate void ReceiveActionEffectDelegate(int sourceId, IntPtr sourceCharacter, IntPtr pos,
+        IntPtr effectHeader, IntPtr effectArray, IntPtr effectTrail);
+
+    private static readonly uint[] SkillIds = { 645, 1652, 11115, 11116 };
+
+    public List<string> CollectionTargets = new();
+    public readonly Stopwatch ClearMarkers = new();
+
+    public ActionEffectHook()
+    {
+        var receiveAEtPtr = Service.SigScanner.ScanText("4C 89 44 24 ?? 55 56 57 41 54 41 55 41 56 48 8D 6C 24");
+        receiveActionEffectHook = new Hook<ReceiveActionEffectDelegate>(receiveAEtPtr, ReceiveActionEffect);
+        receiveActionEffectHook.Enable();
+    }
+
+    public void Dispose()
+    {
+        receiveActionEffectHook.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    private void ReceiveActionEffect(int sourceId, IntPtr sourceCharacter, IntPtr pos, IntPtr effectHeader,
+        IntPtr effectArray, IntPtr effectTrail)
+    {
+        if (!Helper.PlayerExists)
+        {
+            receiveActionEffectHook.Original(sourceId, sourceCharacter, pos, effectHeader, effectArray,
+                effectTrail);
+            return;
+        }
+
+        var id = *((uint*)effectHeader.ToPointer() + 0x2);
+        var targetCount = *(byte*)(effectHeader + 0x21);
+        var targetsParty = false;
+
+        var targetEntries = 1;
+        switch (targetCount)
+        {
+            case 0:
+            case 1:
+                targetEntries = 1;
+                break;
+            case <= 8:
+                targetEntries = 8;
+                break;
+            case <= 16:
+                targetEntries = 16;
+                break;
+            case <= 24:
+                targetEntries = 24;
+                break;
+            case <= 32:
+                targetEntries = 32;
+                break;
+        }
+
+        var targets = new ulong[targetEntries];
+        for (var i = 0; i < targetCount; i++)
+        {
+            targets[i] = *(ulong*)(effectTrail + i * 8);
+            if (Helper.IsIdInParty(targets[i])) targetsParty = true;
+        }
+
+        if (targetsParty)
+            if (SkillIds.Contains(id))
+                foreach (var l in targets)
+                {
+                    if (Helper.IsMarking) continue;
+
+                    if (!ClearMarkers.IsRunning) ClearMarkers.Start();
+
+                    var pMember = Helper.GetPCharFromId((uint)l);
+                    var pMemberName = pMember.Name.ToString();
+                    CollectionTargets.Add(pMemberName);
+
+                    if (CollectionTargets.Count == Helper.JailCount) break;
+                }
+
+        receiveActionEffectHook.Original(sourceId, sourceCharacter, pos, effectHeader, effectArray, effectTrail);
+    }
+}
